609d13a7df23a4d4f8a6eb86a42bb0c146a2a89d
Travis Lawson <travislawclimb@gmail.com>
2020-10-14 06:02:11 -0700

    Finished Start battle method in Battlefield class and added a battlefield object to run the battle.

--- DinosVsRobots/Battlefield.cs
+++ DinosVsRobots/Battlefield.cs
@@ -23,15 +23,35 @@ namespace DinosVsRobots
         public void StartBattle()
         {
 
-            //robot to attack dino
-            fleet1.robotList[0].AttackDino(herd1.dinoList[0]);
-            if (herd1.dinoList[0].health <= 0) //dinosaur runs out of health
+            while (fleet1.robotList.Count > 0 && herd1.dinoList.Count > 0) 
             {
-                // remove from list
-                herd1.dinoList.RemoveAt(0);
+                fleet1.robotList[0].AttackDino(herd1.dinoList[0]);
+                if (herd1.dinoList[0].health <= 0)
+                {
+
+                    herd1.dinoList.RemoveAt(0);
+                    if (herd1.dinoList.Count <= 0)
+                    {
+                        break;
+                    }
+                }
+
+                herd1.dinoList[0].AttackBot(fleet1.robotList[0]);
+                if (fleet1.robotList[0].health <= 0)
+                {
+                    fleet1.robotList.RemoveAt(0);
+                }
             }
-            //dino to attack robot
-            herd1.dinoList[0].AttackBot(fleet1.robotList[0]);
+
+            if (fleet1.robotList.Count > 0)
+            {
+                Console.WriteLine("The awesome Disney robots won the battle!");
+            }
+            else
+            {
+                Console.WriteLine("Somehow the lame dinosaurs won the battle..");
+            }
+
         }
     }
 }
--- DinosVsRobots/Program.cs
+++ DinosVsRobots/Program.cs
@@ -10,8 +10,9 @@ namespace DinosVsRobots
     {
         static void Main(string[] args)
         {
-            
-
+            Battlefield battle1 = new Battlefield();
+            battle1.StartBattle();
+            Console.ReadLine();
         }
     }
 }
